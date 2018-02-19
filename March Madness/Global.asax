90052c6e86f80a7fd2ababb6990285c98a467af4
Greg Padin <padin.greg@gmail.com>
2018-02-19 18:09:50 -0500

    Add project files.

---
+++ March Madness/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="March_Madness.MvcApplication" Language="C#" %>
---
+++ March Madness/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(March_Madness.Startup))]
+namespace March_Madness
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
