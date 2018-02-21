84eed4491fd2c30e23e89b301201ffc57c3d6a90
Greg Padin <padin.greg@gmail.com>
2018-02-21 07:54:50 -0500

    added bracket page and jquery-bracket

---
+++ March Madness/Controllers/BracketController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace March_Madness.Controllers
+{
+    public class BracketController : Controller
+    {
+        // GET: Bracket
+        public ActionResult Index()
+        {
+            return View("BracketForm");
+        }
+    }
+}
