c4817db5f8a1c10e542e72c305833182e42ac89e
Greg Padin <padin.greg@gmail.com>
2018-03-12 06:17:46 -0400

    Making Dapp SQL Free. Just front end needs to be manaaged and for that we can use the mashed keys

--- March Madness/Contracts/contracts/MarchMadness.sol
+++ March Madness/Contracts/contracts/MarchMadness.sol
@@ -10,72 +10,53 @@ contract MarchMadness {
   uint public PoolFee; //what we take
   uint public SalesCommission; //
 
-  mapping(address => bool) ContractAdmins;
+  mapping(address => bytes32) AdminPasswords;
+  mapping(address => bytes32) TempAdminPasswords;
 
-  mapping(uint16 => uint) Pools; //poolId to entry fee in Wei
-  mapping(uint16 => uint) PoolValue; //poolId to poolsTotalEntryFeeCollected
-  mapping(uint16 => address) PoolOwners;
+  mapping(bytes32 => uint) PoolFees; //poolId to entry fee in Wei
+  mapping(bytes32 => uint) PoolValue; //poolId to poolsTotalEntryFeeCollected
+  mapping(bytes32 => bytes32) PoolOwnerPasswords; //an pool to a hash of the pool and the Pool Owner's password
+  mapping(string => bytes32) PoolPublicNames; //Pools Given Public Names to Look them Up
+  mapping(bytes32 => uint8) MaxPoolEntriesPerAddress; //Max Entries Per Address in each pool
+  mapping(bytes32 => uint8) PoolEntriesPerAddress; //Max Entries Per Address in each pool
+  mapping(bytes32 => uint) MaxPoolEntries; //total entries in each pool
+  mapping(bytes32 => uint) PoolEntants; //total entries in each pool
   
 
-  mapping(address => uint16) Entries;  //address to poolID
-  mapping(address => bytes32) BracketStore; //address to bracket
+  mapping(bytes32 => bytes32) BracketStore; //EntryKey to bracket
+  mapping(bytes32 => address) BracketOwner; //maps an EntryKey to its owner
+  mapping(bytes32 => bytes32) BracketEscrow; //Maps the NewOwner/TempPassword with the Bracket they're buying
 
   
   uint TotalPayouts;
   uint TotalCharity; 
 
 
-  //0, "Authorized Withdraw
-  //1, "Winner Payment"
-  //2, Sale
-  enum PaymentCode {
-    AuthorizedWithdraw, 
-    WinnerPayment, 
-    Sale
-    }
-  event PaymentMade(address payee, address payer, PaymentCode indexed paidCode);
+  event PaymentMade(address payee, address payer, string paymentMsg);
   
- 
-
-  // 0, "Insufficient value to cover sale"
-  // 1, "No Bracket Passed for Validation"
-  // 2, "Final and Submitted Bracket Not Equal"
-  // 3, "Not Enough Funds in Contract to Cover TotalPayout"
-  //4, "Entry Fee Not Met"
-  //5, "Pool value at Overflow (currently uint72);
-  //6, "Attempted to sell Empty bracket"
-  //7, "Attempted to sell entry in pool with no payout
-  enum ErrorCode {
-    InsufficientValueForSale, 
-    NoBracketToValidate,
-    SubmittedAndFinalBracketMismatch, 
-    PoolValueGreaterThanTotalPayout,
-	PoolFeeNotMet,
-    EntryFeeNotMet,
-    PoolOverflow,
-    SellingEmptyBracket,
-    SellingFreeTournyBracket
-    }
-  event PaymentError(address attemptedPayee, ErrorCode indexed errCode, string errReason);
-  event BracketError(address entrant, ErrorCode indexed errCode,string errReason);
-  event SaleError(address errParty, ErrorCode indexed errCode, string errReason);
- 
+  event PaymentError(address attemptedPayee, address attemptedPayer, string errMsg);
+  event BracketError(address entrant, string errMsg);
+  event SaleError(address errParty, string errMsg);
 
 
-  event PoolCreated(address owner, uint indexed poolId, uint fee);
+  event PoolCreated(address poolPublicAddress, string poolPublicName, uint fee);
   event BracketCreated(address owner, uint indexed poolId, bytes32 bracket);
+
   mapping(address => uint) charityHold;
   event DonationReceived(address donor, uint amountDonated);
-  event DonationRefunded(address donor, uint amountRefunded, uint donationLeft);
 
+  event AdminError(address attemptAddress, string errMsg);
+  event AdminMessage(string message);
 
-  modifier isPoolOwner(uint16 pool) {
-    require(isAddressPoolOwner(msg.sender, pool));
+  modifier isBracketOwner(string poolAddress, string poolPassword, string bracketPassword, bytes32 ) {
+    var pool = getBracketOwnerHash(msg.sender, poolPassword);
+    getBracketOwnerHash(poolAddress, poolPassword, bracketPassword);
     _;
   }
 
-  modifier isAdmin() {
-    require(ContractAdmins[msg.sender] == true);
+  modifier isAdmin(string password) {
+    require(password != "");
+    require(AdminPasswords[msg.sender] == getPasswordHash(msg.sender, password));
     _;
   }
 
@@ -85,141 +66,165 @@ contract MarchMadness {
   }
 
 
-
-
-
-  function MarchMadness(uint poolFee, uint lockBracketsAfter, uint entryFee, uint16 salesCommission) public {
-    ContractAdmins[msg.sender] = true;
+  function MarchMadness(uint poolFee, uint lockBracketsAfter, uint entryFee, uint16 salesCommission, string password) public {
+    require(bytes(password).length > 8);
+    AdminPasswords[msg.sender] = getPasswordHash(msg.sender, password);
     PoolFee = poolFee;
     EntryFee = entryFee;
     SalesCommission = salesCommission;
     LockTime = now + lockBracketsAfter;
-    
-    OwnershipUpdated(0x0, Owner);
+    AdminMessage("Contract Deployed");
   }
 
-
-
-  function addAdmin(address newAdmin) public isAdmin {
-	require(newAdmin != 0x0);
-    ContractAdmins[newAdmin] = true;
+  function addAdmin(address newAdmin, string tempPassword, string password) public isAdmin(password) {
+	  require(newAdmin != 0x0);
+    TempAdminPasswords[msg.sender] = getPasswordHash(newAdmin, tempPassword);
+    //this will also reset password
   }
 
-  function removeAdmin(address removedAdmin) public isAdmin {
-    require(msg.sender != removedAdmin);
-    ContractAdmins[removedAdmin] = false;
+  function setAdminPassword(string tempPassword, string newPassword) public {
+    require(TempAdminPasswords[msg.sender] == getPasswordHash(msg.sender, tempPassword));
+    require(bytes(newPassword).length > 8);
+    AdminPasswords[msg.sender] = getPasswordHash(msg.sender, newPassword);
+    TempAdminPasswords[msg.sender] == 0x0;
   }
 
-  function updateEntryFee(uint newFee) public isAdmin { 
+  function removeAdmin(address removedAdmin, string password) public isAdmin(password) {
+    require(msg.sender != removedAdmin); //can't remove self from admin
+    AdminPasswords[removedAdmin] = 0x0;
+  }
+
+  function updateEntryFee(uint newFee, string password) public isAdmin(password) { 
     EntryFee = newFee;
   }
 
-  function updatePoolFee(uint newFee) public isAdmin {
+  function updatePoolFee(uint newFee, string password) public isAdmin(password) {
     PoolFee = newFee;
   }
 
-  function updateSalesCommission(uint newFee) public isAdmin {
+  function updateSalesCommission(uint newFee, string password) public isAdmin(password) {
     SalesCommission = newFee;
   }
   
   function secondsUntilLock() public view returns (uint secondsLeft) {
-	return LockTime - now;
+	  return LockTime - now;
   }
 
-  function getContractValue() public view isAdmin returns (uint) {
-  	  return this.balance;
+  
+
+  function getContractValue(string password) public view isAdmin(password) returns (uint balance, uint payoutsDue, uint charity) {
+  	  return (this.balance, TotalPayouts, TotalCharity);
   }
 
-  //handled
-  function withdrawFunds(address withdrawTo, uint amount) public isAdmin returns (bool) {
+  function withdrawFunds(address withdrawTo, uint amount, string password) public isAdmin(password) returns (bool) {
     if (this.balance - (amount) < TotalPayouts) {
-      PaymentError(withdrawTo, ErrorCode.PoolValueGreaterThanTotalPayout, "Not Enough Funds in Contract to Cover TotalPayout");
+      PaymentError(withdrawTo, msg.sender, "Not Enough Funds in Contract to Cover TotalPayout");
+      return false;
+    }
+    if (this.balance - (amount) > this.balance) {
+      PaymentError(withdrawTo, msg.sender, "Underflow of account");
       return false;
     }
 
     if (withdrawTo == 0x0) { 
-      withdrawTo = Owner;
+      PaymentError(withdrawTo, msg.sender, "noWithdrawTo address");
+      return false;
     }
     withdrawTo.transfer(amount);
-    PaymentMade(withdrawTo, address(this), PaymentCode.AuthorizedWithdraw);
+    PaymentMade(withdrawTo, msg.sender, "Administrator Withdrawl");
+    return true;
   }
 
 
-  function createPool(uint16 pool, uint fee) public payable tournamentNotStarted {
-    require(PoolOwners[pool] == 0x0);
+  function createPool(uint fee, uint bonus, uint8 maxEntries, uint8 maxPerAddress, string publicName, string poolPassword, string ownerPassword) public payable tournamentNotStarted {
+    var poolHash = getPasswordHash(msg.sender, poolPassword);
+    require(PoolOwnerPasswords[poolHash] == 0x0); //if this owner has used password before, don't let them reuse it. 
+    require(bytes32(poolPassword).length > 8);
+    require(bytes32(ownerPassword).length > 8);
     if (msg.value < PoolFee) {
-      PaymentError(msg.sender, ErrorCode.PoolFeeNotMet, "Insufficient fee to cover pool creation");
+      PaymentError(msg.sender,  "Insufficient fee to cover pool creation");
       return;
     }
-    Pools[pool] = fee;
-    PoolOwners[pool] = msg.sender;
-    PoolCreated(PoolOwners[pool], pool, Pools[pool]);
+    PoolFees[poolHash] = fee;
+    PoolValue[poolHash] = bonus;
+    MaxPoolEntriesPerAddress[poolHash] = maxPerAddress; //Max Entries Per Address in each pool
+    MaxPoolEntries[poolHash] = maxEntries; //total entries in each pool
+    PoolPublicNames[getPublicHash(msg.sender, publicName)] = poolHash; //makes a unique identifier of the Public Address and the Name. Makes Names not have to be unique, but still lookupable
+    PoolCreated(msg.sender, PoolFees[msg.sender]);
   }
 
-  function getPoolValue(uint16 pool) public view returns (uint) {
-    //require (isAddressContractAdmin(msg.sender) || isAddressPoolOwner(msg.sender, pool) || isAddressInPool(msg.sender, pool));
-    return PoolValue[pool];
+  function getPoolValue(address poolAddress, string publicName) public view returns (uint) {
+    var poolKey = getPublicHash(poolAddress, publicName);
+    return PoolValue[poolKey];
   }
 
-  function getPoolEntryFee(uint16 pool) public view returns(uint) {
-    return Pools[pool];
+  function getPoolEntryFee(address poolOwnerAddress, string poolName) public view returns(uint) {
+    var poolHash = getPoolHash(poolOwnerAddress, poolName);
+    return PoolFees[poolHash];
   }
 
-  //eventually pass the hashed bracked
-  function createBracket(bytes32 bracket, uint16 poolId)  public payable tournamentNotStarted {
-    uint poolEntryFee = Pools[poolId];
-    uint poolValue = PoolValue[poolId];
+
+  //The Entrant will Need to Know the Pool Owner's Address and the Password for that Pool
+  function createBracket(bytes32 bracket, address poolAddress, string poolPassword, string bracketPassword)  public payable tournamentNotStarted {
+    var poolHash = getPoolHash(poolAddress, poolPassword);
+    uint poolEntryFee = PoolFees[poolHash];
+
     if (msg.value < poolEntryFee + EntryFee) {
-      BracketError(msg.sender, ErrorCode.EntryFeeNotMet, "Pool and Entry fee not met");
+      BracketError(msg.sender,  "Pool and Entry fee not met");
       return;
     }
-    
+    require(bytes(bracketPassword).length > 8);
+    uint poolValue = PoolValue[poolHash];
     if (poolValue + poolEntryFee < poolValue) {
-      BracketError(msg.sender, ErrorCode.PoolOverflow, "Pool value would overflow with more brackets");
+      BracketError(msg.sender, "Pool value would overflow with more brackets");
       return;
     }
 
-    Entries[msg.sender] = poolId;
-    BracketStore[msg.sender] = bracket;
-    TotalPayouts += msg.value - EntryFee;
-    PoolValue[poolId] += poolEntryFee;
-    BracketCreated(msg.sender, Entries[msg.sender],BracketStore[msg.sender]);
+    var entryKey = getBracketOwnerHash(poolAddress, poolPassword, bracketPassword);
+    BracketStore[entryKey] = bracket;
+    TotalPayouts += poolEntryFee;
+    PoolValue[poolHash] += poolEntryFee;
+    BracketCreated(msg.sender, poolAddress);
   }
 
 
-  function updateBracket(bytes32 bracket) public tournamentNotStarted {
-    require(Entries[msg.sender] != 0);
-    BracketStore[msg.sender] = bracket;
+  function updateBracket(bytes32 bracket, address poolAddress, string poolPassword, string bracketPassword) public tournamentNotStarted {
+    var entryKey = getBracketOwnerHash(poolAddress, poolPassword, bracketPassword);
+    require(BracketStore[entryKey] != 0);
+    BracketStore[entryKey] = bracket;
   }
- 
 
-  function payWinner(address winner, uint16 pool, bytes32 bracket)  public payable isAdmin returns(bool) {
+
+  function payWinner(address winner, uint16 pool, bytes32 bracket, string password)  public payable isAdmin(password) returns(bool) {
       if (bracket == 0) {
-        PaymentError(winner, ErrorCode.NoBracketToValidate, "No Bracket Passed for Validation");
+        PaymentError(winner, msg.sender, "No Bracket to Validate");
         return false;
       }
       if (BracketStore[winner] != bracket) {
-        PaymentError(winner, ErrorCode.SubmittedAndFinalBracketMismatch, "Final and Submitted Bracket Not Equal");
+        PaymentError(winner, "Final and Submitted Bracket Not Equal");
         return false;
       }
 
       require(PoolValue[pool] > 0);
       winner.transfer(PoolValue[pool]);
-      PaymentMade(winner, address(this), PaymentCode.WinnerPayment);
-      TotalPayouts -= Pools[pool];
+      PaymentMade(winner, msg.sender, "Winner Paid");
+      TotalPayouts -= PoolFees[pool];
       PoolValue[pool] = 0;
   }
 
-  function isAddressContractAdmin(address checkAddress) private view returns (bool) {
-    return checkAddress == Owner;
+  function getPublicHash(address poolAddress, string name) private pure returns(bytes32) {
+    return sha3(poolAddress, name);
+  }
+  function getBracketOwnerHash(address poolAddress, string poolPassword, string bracketPassword) private pure returns (bytes32) {
+    return sha3(poolAddress, poolPassword, bracketPassword, msg.sender);
   }
 
-  function isAddressPoolOwner(address checkAddress, uint16 pool) private view returns (bool) {
-    return PoolOwners[pool] == checkAddress;
+  function getPasswordHash(string password) private view returns (bytes) {
+    return keccak256(msg.sender, password);
   }
 
-  function isAddressInPool(address checkAddress, uint16 pool) private view returns (bool) {
-    return Entries[checkAddress] == pool;
+  function getPoolHash(address poolAddress, string poolPassword) private pure returns (bytes32) {
+    return keccak256(poolAddress, poolPassword);
   }
 
   function () public payable {
